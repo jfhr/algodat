a68acc059fb96161265109f0679e80ae63aee7c0
jfhr <me@jfhr.de>
2021-03-21 20:42:06 +0100

    Add insertion and heap sort

---
+++ Algodat.Test/SortAlgorithmTest.cs
@@ -0,0 +1,34 @@
+using Algodat.SortAlgorithms;
+using NUnit.Framework;
+using System;
+
+namespace Algodat.Test
+{
+    [TestFixture(typeof(HeapSort))]
+    [TestFixture(typeof(InsertionSort))]
+    public class SortAlgorithmTest<T> where T : ISortAlgorithm, new()
+    {
+        [DatapointSource]
+        public int[][] Arrays = new[]
+        {
+            Array.Empty<int>(),
+            new[] { 1 },
+            new[] { 1, 2 },
+            new[] { 2, 1 },
+            new[] { 1, 2, 2, 3 },
+            new[] { 1, 2, 3, 2 },
+            new[] { 1, 1, 1 },
+            new[] { 1, -1, 0 },
+            new[] { 3, 2, 4, 1, 5 },
+        };
+
+        [Theory]
+        public void TestSort(int[] array)
+        {
+            var instance = new T();
+            instance.SortAscending(array);
+
+            Assert.That(array, Is.Ordered.Ascending);
+        }
+    }
+}
---
+++ algodat/SortAlgorithms/HeapSort.cs
@@ -0,0 +1,90 @@
+﻿namespace Algodat.SortAlgorithms
+{
+    public class HeapSort : ISortAlgorithm
+    {
+        public void SortAscending(int[] array)
+        {
+            CreateInstance(array).SortAscending();
+        }
+
+        private Instance CreateInstance(int[] array) => new(array);
+
+        private class Instance
+        {
+            private readonly int[] array;
+
+            /// <summary>
+            /// Size of the heap, this is also the smallest index in the array
+            /// that is not itself part of the heap.
+            /// </summary>
+            private int heapSize;
+
+            public Instance(int[] array)
+            {
+                this.array = array;
+                heapSize = array.Length;
+            }
+
+            public void SortAscending()
+            {
+                BuildMaxHeap();
+
+                while (heapSize > 1)
+                {
+                    Swap(0, heapSize - 1);
+                    heapSize--;
+                    MaxHeapify(0);
+                }
+            }
+
+            /// <summary>
+            /// Turn an arbitrary array into a complete max-heap.
+            /// </summary>
+            private void BuildMaxHeap()
+            {
+                for (int i = array.Length / 2; i >= 0; i--)
+                {
+                    MaxHeapify(i);
+                }
+            }
+
+            /// <summary>
+            /// Ensure that the item at the given index satisfies the max-heap property,
+            /// given that its children already satisfy it.
+            /// </summary>
+            private void MaxHeapify(int index)
+            {
+                int left = index * 2 + 1;
+                if (left >= heapSize)
+                {
+                    return;
+                }
+
+                int right = left + 1;
+                if (right >= heapSize || array[left] >= array[right])
+                {
+                    if (array[index] < array[left])
+                    {
+                        Swap(index, left);
+                        MaxHeapify(left);
+                    }
+                }
+                else
+                {
+                    if (array[index] < array[right])
+                    {
+                        Swap(index, right);
+                        MaxHeapify(right);
+                    }
+                }
+            }
+
+            private void Swap(int a, int b)
+            {
+                int temp = array[a];
+                array[a] = array[b];
+                array[b] = temp;
+            }
+        }
+    }
+}
