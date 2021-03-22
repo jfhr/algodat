ccfbc2f8bb5e12964ad899b916a8de97a86e6350
jfhr <me@jfhr.de>
2021-03-22 13:50:39 +0100

    Move insertion sort to its own file

---
+++ Algodat/SortAlgorithms/InsertionSort.cs
@@ -0,0 +1,43 @@
+﻿namespace Algodat.SortAlgorithms
+{
+    public class InsertionSort : ISortAlgorithm
+    {
+        public void SortAscending(int[] array)
+        {
+            CreateInstance(array).SortAscending();
+        }
+
+        private Instance CreateInstance(int[] array) => new(array);
+
+        private class Instance
+        {
+            private readonly int[] array;
+
+            public Instance(int[] array)
+            {
+                this.array = array;
+            }
+
+            public void SortAscending()
+            {
+                for (int i = 1; i < array.Length; i++)
+                {
+                    for (int j = i; j > 0; j--)
+                    {
+                        if (array[j] < array[j - 1])
+                        {
+                            Swap(j, j - 1);
+                        }
+                    }
+                }
+            }
+
+            private void Swap(int a, int b)
+            {
+                int temp = array[a];
+                array[a] = array[b];
+                array[b] = temp;
+            }
+        }
+    }
+}
