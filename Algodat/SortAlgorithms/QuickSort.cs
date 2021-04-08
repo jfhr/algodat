d4c4139f5aa844e6c488757d17fdb560251473f3
jfhr <me@jfhr.de>
2021-04-08 08:46:59 +0200

    Add quicksort

---
+++ Algodat/SortAlgorithms/QuickSort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Algodat.SortAlgorithms
+{
+    public class QuickSort : ISortAlgorithm
+    {
+        public void SortAscending(int[] array)
+        {
+            SortInternal(array);
+        }
+
+        private void SortInternal(Span<int> span)
+        {
+            if (span.Length <= 1)
+            {
+                return;
+            }
+
+            int pivot = span[^1];
+            int i = -1;
+            for (int j = 0; j < span.Length; j++)
+            {
+                if (span[j] <= pivot)
+                {
+                    if (i + 1 != j)
+                    {
+                        ArrayUtil.Swap(span, i + 1, j);
+                    }
+                    i++;
+                }
+            }
+
+            var left = span[..i];
+            var right = span[(i + 1)..];
+
+            SortInternal(left);
+            SortInternal(right);
+        }
+    }
+}
